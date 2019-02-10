4b1f7e562ec2fe6a261b316614644f3c31e57e3e
Otabek <otabekkamalov@yahoo.com>
2019-02-10 18:49:50 -0500

    Update shuffle.js

--- shuffle.js
+++ shuffle.js
@@ -1,1 +1,19 @@
+//write a function which will accept an array as an argument and each time returns a different shuffled array.
 
+
+const arr = [1,2,3,4,5,6,7,8]
+
+let shuffle = (array) =>{
+  let originalArray = [...array];
+  let newArr = [];
+
+  for (let i=0; i < array.length; i++){
+    let randomNumber =  Math.floor(Math.random() * originalArray.length )
+    newArr.push(originalArray[randomNumber])
+    originalArray.splice(randomNumber, 1)
+}
+  console.log(newArr)
+  return newArr
+}
+
+shuffle(arr)
