908ce8367e883d00ab3a528fee3d2b14b1444091
Otabek <otabekkamalov@yahoo.com>
2019-02-12 23:23:09 -0500

    Update pyramid2.js

--- pyramid2.js
+++ pyramid2.js
@@ -1,1 +1,10 @@
 
+function pyramid(n) {
+  let baseLength = n+(n-1)
+
+  for(let i = 1; i <= n; i++) {
+    let formula = n-i
+    let emptySpacesEachSide = (baseLength - (i+(i-1)))/2
+    console.log(' '.repeat(emptySpacesEachSide) + '#'.repeat(i+(i-1)) + ' '.repeat(emptySpacesEachSide))
+  }
+}
