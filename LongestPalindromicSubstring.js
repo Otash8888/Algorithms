15d5809cfb7b3f6eb18484a36a310a982ce543c6
Otabek <otabekkamalov@yahoo.com>
2019-02-20 01:40:43 -0500

    Update LongestPalindromicSubstring.js

--- LongestPalindromicSubstring.js
+++ LongestPalindromicSubstring.js
@@ -12,3 +12,39 @@ Example 2:
 Input: "cbbd"
 Output: "bb"
 */
+
+// with a bad time complexity
+var findPalindrome = (s) => {
+  let reversed = s.split('').reverse().join('')
+  return reversed === s
+}
+ 
+var longestPalindrome = function(s) {
+  let word = ''
+  let arrayOfPalindromes = []
+  let longestPalindromes = ''
+if (s.length <= 1) {
+    return s
+}
+  for(let i = 0; i < s.length; i++){
+    word = s[i]
+    for (let j = i+1; j < s.length; j++){
+      word +=s[j]
+      if (findPalindrome(word)){
+        arrayOfPalindromes.push(word)
+      }
+      if(j === s.length - 1){
+        word = ''
+      }
+    }
+  }
+    if(arrayOfPalindromes.length){
+        arrayOfPalindromes.sort((a,b)=>b.length - a.length)
+        longestPalindromes = arrayOfPalindromes[0]
+       }
+        
+    if(!longestPalindromes){
+       longestPalindromes = s[0]
+       }
+  return longestPalindromes
+}
