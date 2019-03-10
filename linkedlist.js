3e7ae7026dcde9508543ddc223266aae0feae525
Otabek <otabekkamalov@yahoo.com>
2019-03-10 14:40:31 -0400

    Update linkedlist.js

--- linkedlist.js
+++ linkedlist.js
@@ -1,1 +1,11 @@
 
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
