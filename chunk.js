d8a2d5be659c8bb72d17f32dabdfe92dec31ec6c
Otabek <otabekkamalov@yahoo.com>
2019-03-11 16:44:26 -0400

    Update chunk.js

--- chunk.js
+++ chunk.js
@@ -9,7 +9,6 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-
 function chunk(array, size) {
   const chunked = [];
   let index = 0;
