bd303a076f99354deb2d0272ee10c773b53c0987
Otabek <otabekkamalov@yahoo.com>
2019-04-21 16:04:42 -0400

    Update capitalize.js

--- capitalize.js
+++ capitalize.js
@@ -7,27 +7,29 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {
-  let result = str[0].toUpperCase();
 
-  for (let i = 1; i < str.length; i++) {
-    if (str[i - 1] === ' ') {
-      result += str[i].toUpperCase();
-    } else {
-      result += str[i];
-    }
+
+function capitalize(str) {
+  const words = [];
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
   }
 
-  return result;
+  return words.join(' ');
 }
 
-
 // function capitalize(str) {
-//   const words = [];
-//
-//   for (let word of str.split(' ')) {
-//     words.push(word[0].toUpperCase() + word.slice(1));
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
 //   }
-//
-//   return words.join(' ');
+
+//   return result;
 // }
+
