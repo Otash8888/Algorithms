bc6dee1350d19cb69e8976fd1c33aff9e2521831
Otabek <otabekkamalov@yahoo.com>
2019-04-28 14:56:54 -0400

    Update compare_triplets.js

--- compare_triplets.js
+++ compare_triplets.js
@@ -27,7 +27,16 @@ function readLine() {
 
 // Complete the compareTriplets function below.
 function compareTriplets(a, b) {
-   
+   let alice = 0
+    let bob = 0
+    for (let i = 0; i < 3; i++) {
+        if (a[i] > b[i]) {
+            alice += 1 
+        } else if (a[i] < b[i]) {
+            bob += 1
+        } 
+    }
+    return [alice, bob]
 }
 
 function main() {
