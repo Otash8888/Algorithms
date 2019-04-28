8d3d05e92ae9dbb968cfb579eb5836eb9b760896
Otabek <otabekkamalov@yahoo.com>
2019-04-28 14:55:09 -0400

    Update and rename compare_triplets to compare_triplets.js

---
+++ compare_triplets.js
@@ -0,0 +1,2 @@
+//Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty
+//For example, a = [1,2,3]  and a = [3,2,1] . For elements 0 , Bob is awarded a point because a[0]<b[0]. For the equal elements a[1] and b[1], no points are earned. Finally, for elements 2, a[2]>b[2]  so Alice receives a point. Your return array would be [1,1] with Alice's score first and Bob's second.
