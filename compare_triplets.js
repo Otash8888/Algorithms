0880331775b30040ef277baea82134506b48eac1
Otabek <otabekkamalov@yahoo.com>
2019-04-28 14:55:59 -0400

    Update compare_triplets.js

--- compare_triplets.js
+++ compare_triplets.js
@@ -1,2 +1,45 @@
 //Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty
 //For example, a = [1,2,3]  and a = [3,2,1] . For elements 0 , Bob is awarded a point because a[0]<b[0]. For the equal elements a[1] and b[1], no points are earned. Finally, for elements 2, a[2]>b[2]  so Alice receives a point. Your return array would be [1,1] with Alice's score first and Bob's second.
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+   
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
