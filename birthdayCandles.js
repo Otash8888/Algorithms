2e4690c833b05aac3a481e447dbcb74c410b3cd4
Otabek <otabekkamalov@yahoo.com>
2019-05-26 15:12:09 -0400

    Update birthdayCandles.js

--- birthdayCandles.js
+++ birthdayCandles.js
@@ -3,3 +3,8 @@
 
 // Example
 //For example, if your niece is turning 4 years old, and the cake will have 4 candles of height 4, 4, 1, 3, she will be able to blow out 2 candles successfully, since the tallest candles are of height 4 and there are 2 such candles.
+
+//Function Description
+//Complete the function birthdayCakeCandles in the editor below. It must return an integer representing the number of candles she can blow out.
+// birthdayCakeCandles has the following parameter(s):
+// ar: an array of integers representing candle heights
