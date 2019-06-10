b1a1de3de107f0c7672f6a22032eb5c72d0de4f8
Otabek <otabekkamalov@yahoo.com>
2019-06-09 21:07:33 -0400

    Update sherlockValidString.js

--- sherlockValidString.js
+++ sherlockValidString.js
@@ -1,3 +1,9 @@
 
 // Sherlock considers a string to be valid if all characters of the string appear the same number of times. It is also valid if he can remove just  character at  index in the string, and the remaining characters will occur the same number of times. Given a string , determine if it is valid. If so, return YES, otherwise return NO.
 // For example, if , it is a valid string because frequencies are . So is  because we can remove one  and have  of each character in the remaining string. If  however, the string is not valid as we can only remove  occurrence of . That would leave character frequencies of .
+
+// Function Description
+
+// Complete the isValid function in the editor below. It should return either the string YES or the string NO.
+
+// isValid has the following parameter(s):
