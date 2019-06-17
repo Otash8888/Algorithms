b5bb43e73ea9f4ca69ed3ba883e0fc0cafbe1737
Otabek <otabekkamalov@yahoo.com>
2019-06-16 21:59:39 -0400

    Update appleAndOranges.js

--- appleAndOranges.js
+++ appleAndOranges.js
@@ -23,3 +23,12 @@
 // b: integer, location of the Orange tree.
 // apples: integer array, distances at which each apple falls from the tree.
 // oranges: integer array, distances at which each orange falls from the tree.
+
+
+// Input Format
+
+// The first line contains two space-separated integers denoting the respective values of  and . 
+// The second line contains two space-separated integers denoting the respective values of  and . 
+// The third line contains two space-separated integers denoting the respective values of  and . 
+// The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point . 
+// The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point .
