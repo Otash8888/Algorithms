3cfde3b0fef20faab7b39c3b46a031fbf41ff226
Otabek <otabekkamalov@yahoo.com>
2019-06-20 16:14:13 -0400

    Update strongPassword.js

--- strongPassword.js
+++ strongPassword.js
@@ -1,1 +1,7 @@
+// Louise joined a social networking site to stay in touch with her friends. The signup page required her to input a name and a password. However, the password must be strong. The website considers a password to be strong if it satisfies the following criteria:
 
+// Its length is at least 6.
+// It contains at least one digit.
+// It contains at least one lowercase English character.
+// It contains at least one uppercase English character.
+// It contains at least one special character. The special characters are: !@#$%^&*()-+
