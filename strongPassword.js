fc0dd62e81c39f389a70cf4b2d8601eb0788eeba
Otabek <otabekkamalov@yahoo.com>
2019-06-20 16:23:45 -0400

    Update strongPassword.js

--- strongPassword.js
+++ strongPassword.js
@@ -92,38 +92,36 @@ function minimumNumber(n, password) {
         }
 
     
-        for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
 
-            if (numbersObj[password[i]] && !rtn.number) {
-                rtn.number = true
-                rtn.rtnValue -= 1
-            }
-            if (lower_caseObj[password[i]] && !rtn.lower_case) {
-                rtn.lower_case = true
-                rtn.rtnValue = rtn.rtnValue - 1
-            }
-            if (upper_caseObj[password[i]] && !rtn.upper_case) {
-                rtn.upper_case = true
-                rtn.rtnValue -= 1
-            }
-            if (special_charactersObj[password[i]] && !rtn.special_character) {
-                rtn.special_character = true
-                rtn.rtnValue -= 1
-            }
-        
-             if (n < 6 && 6 - n > rtn.rtnValue) {
-            rtn.rtnValue = 6 - n
+        if (numbersObj[password[i]] && !rtn.number) {
+            rtn.number = true
+            rtn.rtnValue -= 1
+        }
+        if (lower_caseObj[password[i]] && !rtn.lower_case) {
+            rtn.lower_case = true
+            rtn.rtnValue = rtn.rtnValue - 1
+        }
+        if (upper_caseObj[password[i]] && !rtn.upper_case) {
+            rtn.upper_case = true
+            rtn.rtnValue -= 1
+        }
+        if (special_charactersObj[password[i]] && !rtn.special_character) {
+            rtn.special_character = true
+            rtn.rtnValue -= 1
         }
 
-        return rtn.rtnValue
-
-
+         if (n < 6 && 6 - n > rtn.rtnValue) {
+        rtn.rtnValue = 6 - n
         }
-       
-        
 
+    return rtn.rtnValue
 
-
-       
-    
 }
+       
+        
+
+
+
+       
+
