3584de1015708e9f5fc839c7d2fb6a8bd4bf4d41
Otabek <otabekkamalov@yahoo.com>
2019-06-30 13:41:26 -0400

    Update kangaroo.js

--- kangaroo.js
+++ kangaroo.js
@@ -34,7 +34,7 @@
 
 
 function kangaroo(x1, v1, x2, v2) {
-    if (((x1 - x2) % (v2 - v1) == 0) && ((x1 < x2 && v1 > v2))) {
+    if (((x1 - x2) % (v2 - v1) == 0) && ((x1 < x2 && v1 > v2) || (x1 > x2 && v1 < v2))) {
         return 'YES'
     } else {
         return 'NO'
