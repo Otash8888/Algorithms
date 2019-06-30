4930bb051004d8fa60e1f6678ee08761dba2c3ac
Otabek <otabekkamalov@yahoo.com>
2019-06-30 13:33:59 -0400

    Update kangaroo.js

--- kangaroo.js
+++ kangaroo.js
@@ -4,3 +4,5 @@
 // The first kangaroo starts at location  and moves at a rate of  meters per jump.
 // The second kangaroo starts at location  and moves at a rate of  meters per jump.
 // You have to figure out a way to get both kangaroos at the same location at the same time as part of the show. If it is possible, return YES, otherwise return NO.
+
+//For example, kangaroo  starts at  with a jump distance  and kangaroo  starts at  with a jump distance of . After one jump, they are both at , (, ), so our answer is YES.
