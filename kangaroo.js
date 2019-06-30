16e07dc4c74459fa4b84768ba140d535ed3ad027
Otabek <otabekkamalov@yahoo.com>
2019-06-30 13:34:38 -0400

    Update kangaroo.js

--- kangaroo.js
+++ kangaroo.js
@@ -1,8 +1,8 @@
 
 // Description 
 // You are choreographing a circus show with various animals. For one act, you are given two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
-// The first kangaroo starts at location  and moves at a rate of  meters per jump.
-// The second kangaroo starts at location  and moves at a rate of  meters per jump.
+// The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
+// The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
 // You have to figure out a way to get both kangaroos at the same location at the same time as part of the show. If it is possible, return YES, otherwise return NO.
 
 //For example, kangaroo  starts at  with a jump distance  and kangaroo  starts at  with a jump distance of . After one jump, they are both at , (, ), so our answer is YES.
