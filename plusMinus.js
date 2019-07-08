1698a071eb07c23c40c1def11ef6b98544a2f3af
Otabek <otabekkamalov@yahoo.com>
2019-07-07 21:47:44 -0400

    Update plusMinus.js

--- plusMinus.js
+++ plusMinus.js
@@ -1,2 +1,3 @@
 
 //Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. Print the decimal value of each fraction on a new line.
+//This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10 -4 are acceptable.
