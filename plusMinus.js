32dba70999a20f969058af55c169295a55a06ad9
Otabek <otabekkamalov@yahoo.com>
2019-07-07 21:49:16 -0400

    Update plusMinus.js

--- plusMinus.js
+++ plusMinus.js
@@ -1,3 +1,11 @@
 
 //Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. Print the decimal value of each fraction on a new line.
 //This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10 -4 are acceptable.
+
+//Function Description
+
+// Complete the plusMinus function in the editor below. It should print out the ratio of positive, negative and zero items in the array, each on a separate line rounded to six decimals.
+
+// plusMinus has the following parameter(s):
+
+// arr: an array of integers
